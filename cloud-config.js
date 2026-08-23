ca6f9669dad85d3a06a6ad9082d5f1e84c64b60e
gncbzkrt <genco.bozkurt@gmail.com>
2026-08-23 19:49:06 +0300

    Add files via upload

--- cloud-config.js
+++ cloud-config.js
@@ -2,5 +2,5 @@
 // Publishable key tarayıcıda kullanılmak üzere tasarlanmıştır; SECRET/SERVICE_ROLE key BURAYA ASLA YAZILMAZ.
 window.LGS_CLOUD_CONFIG = {
   url: https:"//vzssgzxynvbrswmcumbm.supabase.co",
-  publishableKey: "sb_publishable_TXSFLtTK2cXVAGvIgybqkw_ZETS8QmC"
+  publishableKey:" sb_publishable_TXSFLtTK2cXVAGvIgybqkw_ZETS8QmC"
 };
